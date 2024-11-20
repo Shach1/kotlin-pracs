4727e14876b95a4801e81a78bc445611d2d46eb4
Evgeniy PC <79169380904@mail.ru>
2024-11-20 23:50:51 +0300

    refactor: clear all unused in Color.kt and Theme.kt; update MainActitivity.kt

--- prac8/app/src/main/java/ru/mirea/prac8/MainActivity.kt
+++ prac8/app/src/main/java/ru/mirea/prac8/MainActivity.kt
@@ -1,18 +1,28 @@
 package ru.mirea.prac8
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.mirea.prac8.ui.theme.AppTheme
 import ru.mirea.prac8.ui.theme.AppTypography
 
@@ -21,28 +31,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AppTheme {
-                Greeting()
-            }
+            Greeting()
         }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+    //uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    showSystemUi = true
+    )
 @Composable
 fun Greeting() {
-    Scaffold(Modifier.fillMaxSize()) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .padding(innerPadding)
+    AppTheme {
+        Scaffold(
+            modifier =  Modifier
                 .fillMaxSize()
-        ){
-            Text(
-                text = "Трухманов Евгений Констаноинович ИКБО-35-22",
-                style = AppTypography.displaySmall,
-                modifier = Modifier
-                    .align(Alignment.Center)
-            )
+                .background(color = MaterialTheme.colorScheme.background)
+        ) { innerPadding ->
+
+            Column(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                Spacer(modifier = Modifier.height(100.dp))
+                Box(
+                    modifier = Modifier
+                        .background(
+                            color = MaterialTheme.colorScheme.secondaryContainer,
+                            shape = CircleShape
+                        )
+                ){
+                    Text(
+                        text = "Трухманов Евгений Констаноинович ИКБО-35-22",
+                        color = MaterialTheme.colorScheme.onSecondaryContainer,
+                        style = AppTypography.headlineLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
 }
